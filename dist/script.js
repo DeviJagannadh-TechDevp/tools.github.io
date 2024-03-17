292466adf7baed2379ef3f4b32f090408f4d11ea
Devi Jagannadh Kotha <72683351+DeviJagannadh-TechDevp@users.noreply.github.com>
2024-03-17 14:13:49 +0530

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    if (localStorage.getItem('ribbonCut') != 'cut') {
+        $('.ribbon').delay(2000).fadeIn();
+        $('#imgFollow').delay(2000).fadeIn();
+    }
+
+    $(".ribbon").click(function() {
+        $(".ribbon--l").addClass("hide");
+        $(".ribbon--r").addClass("hide");
+        $(".bow").addClass("hide");
+        $("#imgFollow").hide();
+        localStorage.setItem('ribbonCut', 'cut');
+        
+        // Countdown and confirmation
+        var count = 5;
+        var countdown = setInterval(function() {
+            if (count > 0) {
+                if (count === 5) {
+                    alert("Get ready to witness SU's art website which is 99% complete!");
+                }
+                confirm("Countdown: " + count);
+                count--;
+            } else {
+                clearInterval(countdown);
+                // Redirect to another URL
+                window.location.href = "https://devijagannadh-techdevp.github.io/radhasurekha.github.io/";
+            }
+        }, 1000);
+    });
+});
+
+$(document).mousemove(function(e) {
+    $('#imgFollow').offset({
+        left: e.pageX + 30,
+        top: e.pageY - 20
+    });
+    $('#imgFollow').css('z-index', '1000');
+});
